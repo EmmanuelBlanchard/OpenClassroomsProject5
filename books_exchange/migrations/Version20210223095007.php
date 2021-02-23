c73813f0801d6b31cbfa547cdaf3eafaecb42e4b
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-02-23 10:51:03 +0100

    Generate a migration for the Language entity

---
+++ books_exchange/migrations/Version20210223095007.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210223095007 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE language (id INT AUTO_INCREMENT NOT NULL, parent_id INT DEFAULT NULL, name VARCHAR(30) NOT NULL, slug VARCHAR(255) NOT NULL, INDEX IDX_D4DB71B5727ACA70 (parent_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE language ADD CONSTRAINT FK_D4DB71B5727ACA70 FOREIGN KEY (parent_id) REFERENCES language (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE language DROP FOREIGN KEY FK_D4DB71B5727ACA70');
+        $this->addSql('DROP TABLE language');
+    }
+}
