9ee0081c20bd8f239b86c5abf4946ac8cde21606
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-02-24 13:44:17 +0100

    In the file src/Controller/ResetPasswordController.php: line 160, change of the title for the from property

--- books_exchange/src/Controller/ResetPasswordController.php
+++ books_exchange/src/Controller/ResetPasswordController.php
@@ -157,7 +157,7 @@ class ResetPasswordController extends AbstractController
         }
 
         $email = (new TemplatedEmail())
-            ->from(new Address('no-reply@booksexchange.test', 'Booksexchange.test'))
+            ->from(new Address('no-reply@booksexchange.test', 'BooksExchange.test'))
             ->to($user->getEmail())
             ->subject('Votre demande de réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
