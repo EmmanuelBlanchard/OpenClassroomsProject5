e05529cdd34302e12817063ff86740c277d40fad
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-02 19:18:09 +0100

    In the src/Entity/Book.php file ,line 7: add the use Gedmo\Mapping\Annotation as Gedmo

--- books_exchange/src/Entity/Book.php
+++ books_exchange/src/Entity/Book.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass=BookRepository::class)
