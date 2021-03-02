0003826342c9cafdd6c886bea892b07cf8a1e025
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-02 18:57:04 +0100

    In the src/Controller/AdminController.php, and the addCateogry method, addded the category property that instantitates the Category class and the form property that creates a form with the createForm method from the CategoryFormType class and the category property.In templates/admin, creation of the category folder and in this one, creation of the add.html.twig file.

--- books_exchange/src/Controller/AdminController.php
+++ books_exchange/src/Controller/AdminController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Category;
+use App\Form\CategoryFormType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**
  * @Route("/admin", name="admin_")
@@ -25,10 +28,14 @@ class AdminController extends AbstractController
     /**
      * @Route("/category/add", name="category_add")
      */
-    public function addCategory(): Response
+    public function addCategory(Request $request): Response
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        $category = new Category;
+
+        $form = $this->createForm(CategoryFormType::class, $category);
+        
+        return $this->render('admin/category/add.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
