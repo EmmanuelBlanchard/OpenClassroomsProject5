233fe15198d011e88e0d2e2a052594a5ce16e962
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-03 16:38:46 +0100

    Revert "In the src/Form/BookFormType.php,in the buildForm method: addition to the title field of the TextType class, addtion of the 'category', 'publisher', 'language', 'format', 'state', 'author' fields with the Entity class."
    This reverts commit 73de95e21e5be5bd92d5ebcc1bf6c6c38cba91ee.

--- books_exchange/src/Form/BookFormType.php
+++ books_exchange/src/Form/BookFormType.php
@@ -3,42 +3,22 @@
 namespace App\Form;
 
 use App\Entity\Book;
-use App\Entity\State;
-use App\Entity\Author;
-use App\Entity\Format;
-use App\Entity\Category;
-use App\Entity\Language;
-use App\Entity\Publisher;
-use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class BookFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('category', Entity::class, [
-                'class' => Category::class
-            ])
-            ->add('publisher', Entity::class, [
-                'class' => Publisher::class
-            ])
-            ->add('language', Entity::class, [
-                'class' => Language::class
-            ])
-            ->add('format', Entity::class, [
-                'class' => Format::class
-            ])
-            ->add('state', Entity::class, [
-                'class' => State::class
-            ])
-            ->add('author', Entity::class, [
-                'class' => Author::class
-            ])
+            ->add('title')
+            ->add('category')
+            ->add('publisher')
+            ->add('language')
+            ->add('format')
+            ->add('state')
+            ->add('author')
         ;
     }
 
