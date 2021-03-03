7e309934326b1876032e025b7f4d0afb6936b655
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-03 09:41:50 +0100

    In the src/Entity/language.php file ,line 9: add the use Gedmo\Mapping\Annotation as Gedmo

--- books_exchange/src/Entity/Language.php
+++ books_exchange/src/Entity/Language.php
@@ -6,6 +6,7 @@ use App\Repository\LanguageRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass=LanguageRepository::class)
