f8566170f25f1f394b50179cd8784bcc4695e1ec
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-03 16:48:04 +0100

    In the src/Form/BookFormType.php,in the buildForm method: addition to the title field of the TextType class, addtion of the 'category', 'publisher', 'language', 'format', 'state', 'author' fields with the EntityType class.

--- books_exchange/src/Form/BookFormType.php
+++ books_exchange/src/Form/BookFormType.php
@@ -3,22 +3,42 @@
 namespace App\Form;
 
 use App\Entity\Book;
+use App\Entity\State;
+use App\Entity\Author;
+use App\Entity\Format;
+use App\Entity\Category;
+use App\Entity\Language;
+use App\Entity\Publisher;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class BookFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('category')
-            ->add('publisher')
-            ->add('language')
-            ->add('format')
-            ->add('state')
-            ->add('author')
+            ->add('title', TextType::class)
+            ->add('category', EntityType::class, [
+                'class' => Category::class
+            ])
+            ->add('publisher', EntityType::class, [
+                'class' => Publisher::class
+            ])
+            ->add('language', EntityType::class, [
+                'class' => Language::class
+            ])
+            ->add('format', EntityType::class, [
+                'class' => Format::class
+            ])
+            ->add('state', EntityType::class, [
+                'class' => State::class
+            ])
+            ->add('author', EntityType::class, [
+                'class' => Author::class
+            ])
         ;
     }
 
