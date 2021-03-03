53653847bfb51e6c6075129352647fc3a1397917
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-03 08:44:57 +0100

    In the src/Entity/Publisher.php file ,line 9: add the use Gedmo\Mapping\Annotation as Gedmo

--- books_exchange/src/Entity/Publisher.php
+++ books_exchange/src/Entity/Publisher.php
@@ -6,6 +6,7 @@ use App\Repository\PublisherRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
 
 /**
  * @ORM\Entity(repositoryClass=PublisherRepository::class)
