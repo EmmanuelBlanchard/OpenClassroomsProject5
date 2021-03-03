0ac3f46ee593a5c7a906bdb229ad9bd553d1e5f2
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-03 10:30:57 +0100

    import class language and FormatFormType

---
+++ books_exchange/src/Form/FormatFormType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Format;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormatFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('slug')
+            ->add('parent')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Format::class,
+        ]);
+    }
+}
