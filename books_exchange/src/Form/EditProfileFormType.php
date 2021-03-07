564da109490882f31636616c3d1dc5de4b938334
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-07 19:32:45 +0100

    In the src/Form/EditProfileFormType.php, in the buildForm method, for the 'lastname', 'firstname' fields added 'TextType' class, then  added the 'validate' field and added 'SubmitType' class for this one .

--- books_exchange/src/Form/EditProfileFormType.php
+++ books_exchange/src/Form/EditProfileFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,9 @@ class EditProfileFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('lastname')
-            ->add('firstname')
+            ->add('lastname', TextType::class)
+            ->add('firstname', TextType::class)
+            ->add('validate', SubmitType::class)
         ;
     }
 
