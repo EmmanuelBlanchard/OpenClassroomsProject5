7cc843bdda3b41c828c149a5e9e2585053f14501
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-09 11:33:32 +0100

    In templates/user/inde.html.twig, lines 55-56: added pseudo display, lines 63-66: added zip code and city display. In the src/Form/EditProfileFormType.php, lines 19-22-23-24: in the buildForm method, added 'pseudo','email','zipCode','city' fields.

--- books_exchange/src/Form/EditProfileFormType.php
+++ books_exchange/src/Form/EditProfileFormType.php
@@ -4,18 +4,24 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 
 class EditProfileFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+            ->add('pseudo', TextType::class)
             ->add('lastname', TextType::class)
             ->add('firstname', TextType::class)
+            ->add('email', EmailType::class)
+            ->add('zipCode', IntegerType::class)
+            ->add('city', TextType::class)
             ->add('validate', SubmitType::class)
         ;
     }
