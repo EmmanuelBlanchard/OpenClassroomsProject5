91a381d62e3b9657f3942315f89bd450bb56fed8
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-17 09:35:42 +0100

    In the src/Controller/MainController.php, in the index method added the retrieval of book information with the active field set to true and the exchangeRequest field set to false (adding the createdAt parameter does not work).

--- books_exchange/src/Controller/MainController.php
+++ books_exchange/src/Controller/MainController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Book;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
 {
@@ -13,8 +14,24 @@ class MainController extends AbstractController
      */
     public function index(): Response
     {
+        /*
+        // display all books with true active status and no current exchange request and sort by date of availability in desc 
+        $books = $this->getDoctrine()->getRepository(Book::class)->findBy(
+            ['active' => true],
+            ['exchangeRequest' => false],
+            ['createdAt' => 'desc']
+        );
+        // Invalid order by orientation specified for App\Entity\Book#exchangeRequest
+        */
+
+        //$books = $this->getDoctrine()->getRepository(Book::class)->findBy(array('active' => true, 'exchangeRequest' => false, 'createdAt' => 'DESC'));
+        // Could not convert PHP value 'DESC' of type 'string' to type 'datetime'. Expected one of the following types: null, DateTime
+        
+        $books = $this->getDoctrine()->getRepository(Book::class)->findBy(array('active' => true, 'exchangeRequest' => false));
+        dd($books);
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'controller_name' => 'MainController', 'books' => $books
         ]);
     }
 
