5e45d83912147a6854ade940d35df3c70d292b0e
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-03-27 21:22:53 +0100

    In public/css the style.css and style3.css files , add selector #submitRegistrationFormButton with the display property has none. In public/js/validationregistrationform.js, add the score variable , then of the score === 5, to display the submit button.

--- books_exchange/public/js/validationregistrationform.js
+++ books_exchange/public/js/validationregistrationform.js
@@ -6,6 +6,8 @@ window.onload = () => {
  * This function checks the password
  */
 function checkPassword(){
+    // We initialize the score 
+    let score = 0;
     // We recover what has been entered
     let password = this.value;
 
@@ -19,18 +21,21 @@ function checkPassword(){
     // We check that we have a tiny
     if(/[a-z]/.test(password)){
         tiny.classList.replace("invalid", "valid");
+        score++;
     } else {
         tiny.classList.replace("valid", "invalid");
     }
     // We check that we have a uppercase
     if(/[A-Z]/.test(password)){
         uppercase.classList.replace("invalid", "valid");
+        score++;
     } else {
         uppercase.classList.replace("valid", "invalid");
     }
     // We check that we have a number
     if(/[0-9]/.test(password)){
         number.classList.replace("invalid", "valid");
+        score++;
     } else {
         number.classList.replace("valid", "invalid");
     }
@@ -38,13 +43,22 @@ function checkPassword(){
     /* ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØŒŠþÙÚÛÜÝŸ àáâãäåæçèéêëìíîïðñòóôõöøœšÞùúûüýÿ ¢ß¥£™©®ª×÷±²³¼½¾µ¿¶·¸º°¯§…¤¦≠¬ˆ¨‰  */
     if(/[-_$@!/\\%*#&£~ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØŒŠþÙÚÛÜÝŸàáâãäåæçèéêëìíîïðñòóôõöøœšÞùúûüýÿ¢ß¥£™©®ª×÷±²³¼½¾µ¿¶·¸º°¯§…¤¦≠¬ˆ¨‰]/.test(password)){
         special.classList.replace("invalid", "valid");
+        score++;
     } else {
         special.classList.replace("valid", "invalid");
     }
     // We check that we have the length of the password
     if(password.length >= 12){
         length.classList.replace("invalid", "valid");
+        score++;
     } else {
         length.classList.replace("valid", "invalid");
     }
+
+    if (score === 5){
+        document.getElementById('submitRegistrationFormButton').style.display = "initial";
+    } else {
+        document.getElementById('submitRegistrationFormButton').style.display = "none";
+    }
+    
 }
