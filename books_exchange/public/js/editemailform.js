d8487dbb4bbcafc4b9d3c1ce17af0829758d9776
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-04-04 21:25:02 +0200

    In public/js/editemailform.js, delete the comments and console.log.

--- books_exchange/public/js/editemailform.js
+++ books_exchange/public/js/editemailform.js
@@ -75,91 +75,39 @@ function checkInputs(event){
 		return false;
 	}
 }
-/*
-const inputLabel = document.querySelector('label[for="edit_email_form_email_first"]');
-let newUL = document.createElement('ul');
-inputLabel.insertAdjacentElement('afterend', newUL);
-console.log(inputLabel, 'inpuLabel : ');
-//const inputUl = document.querySelector('form#editemail_form>div.form-control>div#edit_email_form_email>div>ul');
-const inputUl = document.querySelector('div#edit_email_form_email>div>ul');
-console.log(inputUl);
-let newLi = document.createElement('li');
-console.log(newLi);
-inputUl.insertAdjacentElement('afterbegin', newLi);
-const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
-inputLi.textContent = "L\'adresse e-mail ne peut pas être vide";
-*/
-/*
-const inputLabel2 = document.querySelector('label[for="edit_email_form_email_second"]');
-let newUL = document.createElement('ul');
-inputLabel2.insertAdjacentElement('afterend', newUL);
-console.log(inputLabel2, 'inpuLabel : ');
-//const inputUl = document.querySelector('form#editemail_form>div.form-control>div#edit_email_form_email>div>ul');
-const inputUl = document.querySelector('div#edit_email_form_email>div>ul');
-console.log(inputUl);
-let newLi = document.createElement('li');
-console.log(newLi);
-inputUl.insertAdjacentElement('afterbegin', newLi);
-const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
-inputLi.textContent = "L\'adresse e-mail repetée ne peut pas être vide";
-*/
 
 function setErrorFor(input, message) {
 	const formControl = input.parentElement;
-	//const small = formControl.querySelector('small');
-    //const small = document.querySelector('#editpassword_form>div.form-control>small');
 	formControl.className = 'form-control error';
-	//small.innerText = message;
-    
-    // Test
     const inputLabel = document.querySelector('label[for="edit_email_form_email_first"]');
     const inputLabel2 = document.querySelector('label[for="edit_email_form_email_second"]');
-    
-    //console.log(input,'Input dans setErrorFor');
-
     const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
     
     if(input === email) {
-        console.log('Dans input === email !');
         if (inputLi === null) {
             let newUL = document.createElement('ul');
-            console.log(newUL);
             inputLabel.insertAdjacentElement('afterend', newUL);
-            console.log(inputLabel);
-            console.log(input, 'Dans input === email et inpuLi === null !');
             const inputUl = document.querySelector('div#edit_email_form_email>div>ul');
-            console.log(inputUl , 'InputUl crée : ?');
             let newLi = document.createElement('li');
             inputUl.insertAdjacentElement('afterbegin', newLi);
             const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
-            //inputLi.innerTexT = message;
             inputLi.textContent = message;
         } else {
-            console.log('Dans input === email et inpuLi !== null');
-            //inputLi.innerTexT = message;
             inputLi.textContent = message;
         }
-        
-    } else if(input === email2) {
-        //console.log('Dans input === email2');
+    } else {
         if (inputLi === null) {
-            console.log(input, 'Dans input === email2 et inpuLi === null !');
             let newUL = document.createElement('ul');
             inputLabel2.insertAdjacentElement('afterend', newUL);
             const inputUl = document.querySelector('div#edit_email_form_email>div>ul');
             let newLi = document.createElement('li');
             inputUl.insertAdjacentElement('afterbegin', newLi);
             const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
-            //inputLi.innerTexT = message;
             inputLi.textContent = message;
         }
         else {
-            //console.log('Dans input === email2 et inpuLi !== null');
-            //inputLi.innerTexT = message;
             inputLi.textContent = message;
         }
-    } else {
-        console.log('dans else');
     }
 }
 
