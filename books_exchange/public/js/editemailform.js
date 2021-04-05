39c0236c53966b6a797804320db7408b5d850428
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-04-05 11:35:13 +0200

    Re-indent the editpasswordform.js and editemailform.js files.

--- books_exchange/public/js/editemailform.js
+++ books_exchange/public/js/editemailform.js
@@ -1,123 +1,123 @@
 window.onload = () => {
-    email.addEventListener('input', checkInputEmail);
-    email2.addEventListener('input', checkInputEmail2);
+  email.addEventListener('input', checkInputEmail);
+  email2.addEventListener('input', checkInputEmail2);
 }
 
 let form = document.querySelector('#editemail_form');
 const email = document.querySelector('#edit_email_form_email_first');
 const email2 = document.querySelector('#edit_email_form_email_second');
 
-function checkInputEmail(){
-	// trim to remove the whitespaces
-	const emailValue = email.value.trim();
+function checkInputEmail() {
+  // trim to remove the whitespaces
+  const emailValue = email.value.trim();
 
-    if(emailValue === '') {
-		setErrorFor(email, 'L\'adresse e-mail ne peut pas être vide');
-	} else if (!isEmail(emailValue)) {
-		setErrorFor(email, 'L\'adresse e-mail n\'est pas valide, elle doit comporter au moins une arobase, un caractère point puis au minimun deux caractères');
-    } else {
-		setSuccessFor(email);
-	}
+  if (emailValue === '') {
+    setErrorFor(email, 'L\'adresse e-mail ne peut pas être vide');
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'L\'adresse e-mail n\'est pas valide, elle doit comporter au moins une arobase, un caractère point puis au minimun deux caractères');
+  } else {
+    setSuccessFor(email);
+  }
 }
 
-function checkInputEmail2(){
-	// trim to remove the whitespaces
-	const emailValue = email.value.trim();
-    const email2Value = email2.value.trim();
+function checkInputEmail2() {
+  // trim to remove the whitespaces
+  const emailValue = email.value.trim();
+  const email2Value = email2.value.trim();
 
-    if(email2Value === '') {
-		setErrorFor(email2, 'L\'adresse e-mail répétée ne peut pas être vide');
-	} else if (!isEmail(email2Value)) {
-		setErrorFor(email2, 'L\'adresse e-mail n\'est pas valide, elle doit comporter au moins une arobase, un caractère point puis au minimun deux caractères');
-    } else if(emailValue !== email2Value) {
-		setErrorFor(email2, 'Les adresses e-mail ne correspondent pas');
-	} else {
-		setSuccessFor(email2);
-	}
+  if (email2Value === '') {
+    setErrorFor(email2, 'L\'adresse e-mail répétée ne peut pas être vide');
+  } else if (!isEmail(email2Value)) {
+    setErrorFor(email2, 'L\'adresse e-mail n\'est pas valide, elle doit comporter au moins une arobase, un caractère point puis au minimun deux caractères');
+  } else if (emailValue !== email2Value) {
+    setErrorFor(email2, 'Les adresses e-mail ne correspondent pas');
+  } else {
+    setSuccessFor(email2);
+  }
 }
 
 form.addEventListener('submit', event => {
-	checkInputs(event);
+  checkInputs(event);
 });
 
-function checkInputs(event){
-	// trim to remove the whitespaces
-	const emailValue = email.value.trim();
-    const email2Value = email2.value.trim();
+function checkInputs(event) {
+  // trim to remove the whitespaces
+  const emailValue = email.value.trim();
+  const email2Value = email2.value.trim();
 
-	// We initialize the score
-	let score = 0;
-	
-	if(emailValue === '') {
-		setErrorFor(email, 'L\'adresse e-mail ne peut pas être vide');
-	} else if (!isEmail(emailValue)) {
-		setErrorFor(email, 'L\'adresse e-mail n\'est pas valide, elle doit comporter au moins une arobase, un caractère point puis au minimun deux caractères');
-    } else {
-		score++;
-		setSuccessFor(email);
-	}
+  // We initialize the score
+  let score = 0;
 
-    if(email2Value === '') {
-		setErrorFor(email2, 'L\'adresse e-mail répétée ne peut pas être vide');
-	} else if (!isEmail(email2Value)) {
-		setErrorFor(email2, 'L\'adresse e-mail n\'est pas valide, elle doit comporter au moins une arobase, un caractère point puis au minimun deux caractères');
-    } else if(emailValue !== email2Value) {
-		setErrorFor(email2, 'Les adresses e-mail ne correspondent pas');
-	} else {
-		score++;
-		setSuccessFor(email2);
-	}
+  if (emailValue === '') {
+    setErrorFor(email, 'L\'adresse e-mail ne peut pas être vide');
+  } else if (!isEmail(emailValue)) {
+    setErrorFor(email, 'L\'adresse e-mail n\'est pas valide, elle doit comporter au moins une arobase, un caractère point puis au minimun deux caractères');
+  } else {
+    score++;
+    setSuccessFor(email);
+  }
 
-	if(score === 2) {
-		return true;
-	} else {
-		event.preventDefault();
-		return false;
-	}
+  if (email2Value === '') {
+    setErrorFor(email2, 'L\'adresse e-mail répétée ne peut pas être vide');
+  } else if (!isEmail(email2Value)) {
+    setErrorFor(email2, 'L\'adresse e-mail n\'est pas valide, elle doit comporter au moins une arobase, un caractère point puis au minimun deux caractères');
+  } else if (emailValue !== email2Value) {
+    setErrorFor(email2, 'Les adresses e-mail ne correspondent pas');
+  } else {
+    score++;
+    setSuccessFor(email2);
+  }
+
+  if (score === 2) {
+    return true;
+  } else {
+    event.preventDefault();
+    return false;
+  }
 }
 
 function setErrorFor(input, message) {
-	const formControl = input.parentElement;
-	formControl.className = 'form-control error';
-    const inputLabel = document.querySelector('label[for="edit_email_form_email_first"]');
-    const inputLabel2 = document.querySelector('label[for="edit_email_form_email_second"]');
-    const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
-    
-    if(input === email) {
-        if (inputLi === null) {
-            let newUL = document.createElement('ul');
-            inputLabel.insertAdjacentElement('afterend', newUL);
-            const inputUl = document.querySelector('div#edit_email_form_email>div>ul');
-            let newLi = document.createElement('li');
-            inputUl.insertAdjacentElement('afterbegin', newLi);
-            const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
-            inputLi.textContent = message;
-        } else {
-            inputLi.textContent = message;
-        }
+  const formControl = input.parentElement;
+  formControl.className = 'form-control error';
+  const inputLabel = document.querySelector('label[for="edit_email_form_email_first"]');
+  const inputLabel2 = document.querySelector('label[for="edit_email_form_email_second"]');
+  const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
+
+  if (input === email) {
+    if (inputLi === null) {
+      let newUL = document.createElement('ul');
+      inputLabel.insertAdjacentElement('afterend', newUL);
+      const inputUl = document.querySelector('div#edit_email_form_email>div>ul');
+      let newLi = document.createElement('li');
+      inputUl.insertAdjacentElement('afterbegin', newLi);
+      const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
+      inputLi.textContent = message;
     } else {
-        if (inputLi === null) {
-            let newUL = document.createElement('ul');
-            inputLabel2.insertAdjacentElement('afterend', newUL);
-            const inputUl = document.querySelector('div#edit_email_form_email>div>ul');
-            let newLi = document.createElement('li');
-            inputUl.insertAdjacentElement('afterbegin', newLi);
-            const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
-            inputLi.textContent = message;
-        }
-        else {
-            inputLi.textContent = message;
-        }
+      inputLi.textContent = message;
     }
+  } else {
+    if (inputLi === null) {
+      let newUL = document.createElement('ul');
+      inputLabel2.insertAdjacentElement('afterend', newUL);
+      const inputUl = document.querySelector('div#edit_email_form_email>div>ul');
+      let newLi = document.createElement('li');
+      inputUl.insertAdjacentElement('afterbegin', newLi);
+      const inputLi = document.querySelector('div#edit_email_form_email>div>ul>li');
+      inputLi.textContent = message;
+    }
+    else {
+      inputLi.textContent = message;
+    }
+  }
 }
 
 function setSuccessFor(input) {
-	const formControl = input.parentElement;
-	formControl.className = 'form-control success';
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
 }
 
 function isEmail(email) {
-	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
 /*
@@ -129,19 +129,19 @@ https://www.regular-expressions.info/email.html
 
 ^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$
 
-^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,63}$ 
+^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,63}$
 
-^[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$ 
+^[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$
 
-^(?=[A-Z0-9@._%+-]{6,254}$)[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,8}[A-Z]{2,63}$ 
+^(?=[A-Z0-9@._%+-]{6,254}$)[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,8}[A-Z]{2,63}$
 
 ^[A-Z0-9][A-Z0-9._%+-]{0,63}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$
 
 ^(?=[A-Z0-9][A-Z0-9@._%+-]{5,253}$)
-[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,8}[A-Z]{2,63}$ 
+[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,8}[A-Z]{2,63}$
 
 ^[A-Z0-9][A-Z0-9._%+-]{0,63}@
-(?:[A-Z0-9](?:[A-Z0-9-]{0,62}[A-Z0-9])?\.){1,8}[A-Z]{2,63}$ 
+(?:[A-Z0-9](?:[A-Z0-9-]{0,62}[A-Z0-9])?\.){1,8}[A-Z]{2,63}$
 
 ^[A-Z0-9][A-Z0-9._%+-]{0,63}@
 (?:(?=[A-Z0-9-]{1,63}\.)[A-Z0-9]+(?:-[A-Z0-9]+)*\.){1,8}[A-Z]{2,63}$
--- books_exchange/public/js/editpasswordform.js
+++ books_exchange/public/js/editpasswordform.js
@@ -1,122 +1,119 @@
 window.onload = () => {
-    password.addEventListener('input', checkInputPassword);
-    password2.addEventListener('input', checkInputPassword2);
+  password.addEventListener('input', checkInputPassword);
+  password2.addEventListener('input', checkInputPassword2);
 }
 
 let form = document.querySelector('#editpassword_form');
 const password = document.querySelector('#change_password_form_plainPassword_first');
 const password2 = document.querySelector('#change_password_form_plainPassword_second');
 
-function checkInputPassword(){
-	// trim to remove the whitespaces
-	const passwordValue = password.value.trim();
+function checkInputPassword() {
+  // trim to remove the whitespaces
+  const passwordValue = password.value.trim();
 
-	if(passwordValue === '') {
-		setErrorFor(password, 'Le mot de passe ne peut pas être vide');
-	} else if (!isPasswordTiny(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe doit comporter au moins une lettre minuscule');
-    } else if (!isPasswordUppercase(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe doit comporter au moins une une lettre majuscule');
-    }
-    else if (!isPasswordNumber(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe doit comporter au moins un chiffre');
-    }
-    else if (!isPasswordSpecial(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe doit comporter au moins un caractère spécial');
-    }
-    else if (!isPasswordLength(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe doit comporter au moins 12 caractères');
-    } else if (!isPassword(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe n\'est pas valide, il doit comporter au moins une lettre minuscule, une lettre majuscule, un chiffre, un caractère spécial et 12 caractères minimun');
-    } else {
-		setSuccessFor(password);
-	}
+  if (passwordValue === '') {
+    setErrorFor(password, 'Le mot de passe ne peut pas être vide');
+  } else if (!isPasswordTiny(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe doit comporter au moins une lettre minuscule');
+  } else if (!isPasswordUppercase(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe doit comporter au moins une une lettre majuscule');
+  } else if (!isPasswordNumber(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe doit comporter au moins un chiffre');
+  } else if (!isPasswordSpecial(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe doit comporter au moins un caractère spécial');
+  } else if (!isPasswordLength(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe doit comporter au moins 12 caractères');
+  } else if (!isPassword(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe n\'est pas valide, il doit comporter au moins une lettre minuscule, une lettre majuscule, un chiffre, un caractère spécial et 12 caractères minimun');
+  } else {
+    setSuccessFor(password);
+  }
 }
 
-function checkInputPassword2(){
-	// trim to remove the whitespaces
-	const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
+function checkInputPassword2() {
+  // trim to remove the whitespaces
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
 
-    if(password2Value === '') {
-		setErrorFor(password2, 'Le mot de passe répété ne peut pas être vide');
-	} else if (!isPasswordTiny(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété doit comporter au moins une lettre minuscule');
-    } else if (!isPasswordUppercase(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété doit comporter au moins une une lettre majuscule');
-    } else if (!isPasswordNumber(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété doit comporter au moins un chiffre');
-    } else if (!isPasswordSpecial(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété doit comporter au moins un caractère spécial');
-    } else if (!isPasswordLength(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété doit comporter au moins 12 caractères');
-    } else if (!isPassword(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété n\'est pas valide, il doit comporter au moins une lettre minuscule, une lettre majuscule, un chiffre, un caractère spécial et 12 caractères minimun');
-    } else if(passwordValue !== password2Value) {
-		setErrorFor(password2, 'Les mots de passe ne correspondent pas');
-	} else {
-		setSuccessFor(password2);
-	}
+  if (password2Value === '') {
+    setErrorFor(password2, 'Le mot de passe répété ne peut pas être vide');
+  } else if (!isPasswordTiny(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété doit comporter au moins une lettre minuscule');
+  } else if (!isPasswordUppercase(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété doit comporter au moins une une lettre majuscule');
+  } else if (!isPasswordNumber(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété doit comporter au moins un chiffre');
+  } else if (!isPasswordSpecial(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété doit comporter au moins un caractère spécial');
+  } else if (!isPasswordLength(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété doit comporter au moins 12 caractères');
+  } else if (!isPassword(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété n\'est pas valide, il doit comporter au moins une lettre minuscule, une lettre majuscule, un chiffre, un caractère spécial et 12 caractères minimun');
+  } else if (passwordValue !== password2Value) {
+    setErrorFor(password2, 'Les mots de passe ne correspondent pas');
+  } else {
+    setSuccessFor(password2);
+  }
 }
 
 form.addEventListener('submit', event => {
-	checkInputs(event);
+  checkInputs(event);
 });
 
-function checkInputs(event){
-	// trim to remove the whitespaces
-	const passwordValue = password.value.trim();
-    const password2Value = password2.value.trim();
+function checkInputs(event) {
+  // trim to remove the whitespaces
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
 
-	// We initialize the score
-	let score = 0;
-	
-	if(passwordValue === '') {
-		setErrorFor(password, 'Le mot de passe ne peut pas être vide');
-	} else if (!isPasswordTiny(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe doit comporter au moins une lettre minuscule');
-    } else if (!isPasswordUppercase(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe doit comporter au moins une une lettre majuscule');
-    } else if (!isPasswordNumber(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe doit comporter au moins un chiffre');
-    } else if (!isPasswordSpecial(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe doit comporter au moins un caractère spécial');
-    } else if (!isPasswordLength(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe doit comporter au moins 12 caractères');
-    } else if (!isPassword(passwordValue)) {
-		setErrorFor(password, 'Le mot de passe n\'est pas valide, il doit comporter au moins une lettre minuscule, une lettre majuscule, un chiffre, un caractère spécial et 12 caractères minimun');
-    } else {
-		score++;
-		setSuccessFor(password);
-	}
+  // We initialize the score
+  let score = 0;
 
-    if(password2Value === '') {
-		setErrorFor(password2, 'Le mot de passe répété ne peut pas être vide');
-	} else if (!isPasswordTiny(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété doit comporter au moins une lettre minuscule');
-    } else if (!isPasswordUppercase(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété doit comporter au moins une une lettre majuscule');
-    } else if (!isPasswordNumber(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété doit comporter au moins un chiffre');
-    } else if (!isPasswordSpecial(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété doit comporter au moins un caractère spécial');
-    } else if (!isPasswordLength(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété doit comporter au moins 12 caractères');
-    } else if (!isPassword(password2Value)) {
-		setErrorFor(password2, 'Le mot de passe répété n\'est pas valide, il doit comporter au moins une lettre minuscule, une lettre majuscule, un chiffre, un caractère spécial et 12 caractères minimun');
-    } else if(passwordValue !== password2Value) {
-		setErrorFor(password2, 'Les mots de passe ne correspondent pas');
-	} else {
-		score++;
-		setSuccessFor(password2);
-	}
+  if (passwordValue === '') {
+    setErrorFor(password, 'Le mot de passe ne peut pas être vide');
+  } else if (!isPasswordTiny(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe doit comporter au moins une lettre minuscule');
+  } else if (!isPasswordUppercase(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe doit comporter au moins une une lettre majuscule');
+  } else if (!isPasswordNumber(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe doit comporter au moins un chiffre');
+  } else if (!isPasswordSpecial(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe doit comporter au moins un caractère spécial');
+  } else if (!isPasswordLength(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe doit comporter au moins 12 caractères');
+  } else if (!isPassword(passwordValue)) {
+    setErrorFor(password, 'Le mot de passe n\'est pas valide, il doit comporter au moins une lettre minuscule, une lettre majuscule, un chiffre, un caractère spécial et 12 caractères minimun');
+  } else {
+    score++;
+    setSuccessFor(password);
+  }
 
-	if(score === 2) {
-		return true;
-	} else {
-		event.preventDefault();
-		return false;
-	}
+  if (password2Value === '') {
+    setErrorFor(password2, 'Le mot de passe répété ne peut pas être vide');
+  } else if (!isPasswordTiny(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété doit comporter au moins une lettre minuscule');
+  } else if (!isPasswordUppercase(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété doit comporter au moins une une lettre majuscule');
+  } else if (!isPasswordNumber(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété doit comporter au moins un chiffre');
+  } else if (!isPasswordSpecial(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété doit comporter au moins un caractère spécial');
+  } else if (!isPasswordLength(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété doit comporter au moins 12 caractères');
+  } else if (!isPassword(password2Value)) {
+    setErrorFor(password2, 'Le mot de passe répété n\'est pas valide, il doit comporter au moins une lettre minuscule, une lettre majuscule, un chiffre, un caractère spécial et 12 caractères minimun');
+  } else if (passwordValue !== password2Value) {
+    setErrorFor(password2, 'Les mots de passe ne correspondent pas');
+  } else {
+    score++;
+    setSuccessFor(password2);
+  }
+
+  if (score === 2) {
+    return true;
+  } else {
+    event.preventDefault();
+    return false;
+  }
 }
 
 function setErrorFor(input, message) {
@@ -126,8 +123,8 @@ function setErrorFor(input, message) {
   const inputLabel = document.querySelector('label[for="change_password_form_plainPassword_first"]');
   const inputLabel2 = document.querySelector('label[for="change_password_form_plainPassword_second"]');
   const inputLi = document.querySelector('div#change_password_form_plainPassword>div>ul>li');
-  
-  if(input === password) {
+
+  if (input === password) {
     if (inputLi === null) {
       let newUL = document.createElement('ul');
       inputLabel.insertAdjacentElement('afterend', newUL);
@@ -155,34 +152,34 @@ function setErrorFor(input, message) {
 }
 
 function setSuccessFor(input) {
-	const formControl = input.parentElement;
-	formControl.className = 'form-control success';
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
 }
 
 function isPasswordTiny(password) {
-    return /[a-z]/.test(password);
+  return /[a-z]/.test(password);
 }
 
 function isPasswordUppercase(password) {
-    return /[A-Z]/.test(password);
+  return /[A-Z]/.test(password);
 }
 
 function isPasswordNumber(password) {
-	return /\d/.test(password);
+  return /\d/.test(password);
 }
 
 function isPasswordSpecial(password) {
-    return /[-_?+!*$@%_&~`\/\\^\|\#{}()\[\]#£ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØŒŠþÙÚÛÜÝŸàáâãäåæçèéêëìíîïðñòóôõöøœšÞùúûüýÿ¢ß¥£™©®ª×÷±²³¼½¾µ¿¶·¸º°¯§…¤¦≠¬ˆ¨‰]/.test(password);
+  return /[-_?+!*$@%_&~`\/\\^\|\#{}()\[\]#£ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØŒŠþÙÚÛÜÝŸàáâãäåæçèéêëìíîïðñòóôõöøœšÞùúûüýÿ¢ß¥£™©®ª×÷±²³¼½¾µ¿¶·¸º°¯§…¤¦≠¬ˆ¨‰]/.test(password);
 }
 
 function isPasswordLength(password) {
-    if(password.length >= 12) {
-        return true;
-    } else {
-        return false;
-    }
+  if (password.length >= 12) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function isPassword(password) {
-    return /^(?=.+[A-Z])(?=.+[a-z])(?=.+\d)(?=.+[-_?+!*$@%_&~`\/\\^\|\#{}()\[\]#£ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØŒŠþÙÚÛÜÝŸàáâãäåæçèéêëìíîïðñòóôõöøœšÞùúûüýÿ¢ß¥£™©®ª×÷±²³¼½¾µ¿¶·¸º°¯§…¤¦≠¬ˆ¨‰])([-_?+!*$@%_&~`\/\\^\|\#{}()\[\]#£ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØŒŠþÙÚÛÜÝŸàáâãäåæçèéêëìíîïðñòóôõöøœšÞùúûüýÿ¢ß¥£™©®ª×÷±²³¼½¾µ¿¶·¸º°¯§…¤¦≠¬ˆ¨‰\w]{12,})$/.test(password);
+  return /^(?=.+[A-Z])(?=.+[a-z])(?=.+\d)(?=.+[-_?+!*$@%_&~`\/\\^\|\#{}()\[\]#£ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØŒŠþÙÚÛÜÝŸàáâãäåæçèéêëìíîïðñòóôõöøœšÞùúûüýÿ¢ß¥£™©®ª×÷±²³¼½¾µ¿¶·¸º°¯§…¤¦≠¬ˆ¨‰])([-_?+!*$@%_&~`\/\\^\|\#{}()\[\]#£ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØŒŠþÙÚÛÜÝŸàáâãäåæçèéêëìíîïðñòóôõöøœšÞùúûüýÿ¢ß¥£™©®ª×÷±²³¼½¾µ¿¶·¸º°¯§…¤¦≠¬ˆ¨‰\w]{12,})$/.test(password);
 }
