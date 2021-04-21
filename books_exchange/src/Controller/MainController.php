6d730695795b204e856944c90dded482ea02dbed
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-04-21 09:51:13 +0200

    In src/Controller/MainController.php, update index method: delete the use for Criteria and return to the method before.

--- books_exchange/src/Controller/MainController.php
+++ books_exchange/src/Controller/MainController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Book;
-use Doctrine\Common\Collections\Criteria;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,20 +13,8 @@ class MainController extends AbstractController
      * @Route("/", name="app_home")
      */
     public function index(): Response
-    {   $user = $this->getUser();
-        //$books = $this->getDoctrine()->getRepository(Book::class)->findBy(['active' => true, 'exchangeRequest' => false]);
-        // test does not work
-        //$books = $this->getDoctrine()->getRepository(Book::class)->findBy(['active' => true, 'exchangeRequest' => false, 'user' => !$user]);
-        //dd($books);
-        // test does not work
-        // Add a not equals parameter to your criteria
-        $criteria = new Criteria();
-        $criteria->where(Criteria::expr()->neq('user', $user));
-        // Find all from the repository matching your criteria
-        //$result = $entityRepository->matching($criteria);
-
-        $books = $this->getDoctrine()->getRepository(Book::class);
-        $result = $books->matching($criteria);
+    {
+        $books = $this->getDoctrine()->getRepository(Book::class)->findBy(['active' => true, 'exchangeRequest' => false]);
         
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController', 'books' => $books
