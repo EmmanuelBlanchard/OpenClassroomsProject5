b5647b9e1ea781410248e539bef0cb625b9ebdd7
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-04-22 14:17:38 +0200

    In src/ folder, create Extensions/ folder then create Doctrine/ folder then create MatchAgainst.php file to allow a match against inside the repository.

---
+++ books_exchange/migrations/Version20210422120042.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210422120042 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE FULLTEXT INDEX IDX_CBE5A3312B36786BCE286663 ON book (title, summary)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP INDEX IDX_CBE5A3312B36786BCE286663 ON book');
+    }
+}
