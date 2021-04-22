b43fd7b071fddea6de4e9e2fedd648cf213d4524
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-04-22 16:29:27 +0200

    In src/Form/SearchFormType/php, update the buildForm method with rename the 'field_name' field by 'words' field with SearchType class and the 'label' is false and the 'attr' added 'class' with 'form-control mme-2' and 'placeholder' with 'Rechercher' then added 'search' field with SubmitType class and the 'label' is 'Rechercher' and the 'attr' added 'class' with 'btn btn-outline-orange'.

--- books_exchange/src/Form/SearchBookFormType.php
+++ books_exchange/src/Form/SearchBookFormType.php
@@ -5,13 +5,27 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SearchBookFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
+            ->add('words', SearchType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-control me-2',
+                    'placeholder' => 'Rechercher'
+                ]
+            ])
+            ->add('search', SubmitType::class, [
+                'label' => 'Rechercher',
+                'attr' => [
+                    'class' => 'btn btn-outline-orange',
+                ]
+            ])
         ;
     }
 
