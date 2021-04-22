8279a0c92b9c7faf002ea6db6d207f26e968f03d
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-04-22 16:39:24 +0200

    In src/Controller/MainController.php,update the index method with added Request class in two parameter of the method, added form property wich created the formand added 'searchBookForm' for the two parameter in the array of the render method."

--- books_exchange/src/Controller/MainController.php
+++ books_exchange/src/Controller/MainController.php
@@ -2,20 +2,28 @@
 
 namespace App\Controller;
 
+use App\Form\SearchBookFormType;
 use App\Repository\BookRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class MainController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
      */
-    public function index(BookRepository $bookRepo): Response
+    public function index(BookRepository $bookRepo, Request $request): Response
     {
+        $form = $this->createForm(SearchBookFormType::class);
+
+        $search = $form->handleRequest($request);
+
         return $this->render('main/index.html.twig', [
-            'books' => $bookRepo->findBy(['active' => true, 'exchangeRequest' => false], ['createdAt' => 'desc'], 10),
+            'books' => $bookRepo->findBy(['active' => true, 'exchangeRequest' => false],
+            ['createdAt' => 'desc'], 10),
+            'searchBookForm' => $form->createView()
         ]);
 
     }
