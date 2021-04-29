352263f7f51b667352c0334687606fbd4ae3440b
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-04-29 14:26:23 +0200

    In templates/admin/book/index.html.twig, line 78: added data-author, data-publisher, data-format, data-state attributes then added aria-haspopup and aria-controls properties, line 88: change the value of aria-labelledy property to 'dialog-title', line 89: added to the <div> tag the role='document', line 92: change the value of id property to 'dialog-title', line 93: change the value aria-label property to 'Fermer', line 97: added aria-label, title, aria-controls, role properties, line 98: added aria-label, title properties. In public/js/modalbook.js, line 8: add the values of the attributes data-author data-publisher, data-format, data-state in the value of the property innerText.

--- books_exchange/public/js/modalbook.js
+++ books_exchange/public/js/modalbook.js
@@ -5,7 +5,7 @@ window.onload = () => {
     for (let button of deletion) {
         button.addEventListener("click", function () {
             document.querySelector(".modal-footer a").href = `/admin/book/delete/${this.dataset.id}`
-            document.querySelector(".modal-body").innerText = `Êtes-vous sûr de vouloir supprimer le livre "${this.dataset.title}"`
+            document.querySelector(".modal-body").innerText = `Êtes-vous sûr de vouloir supprimer le livre "${this.dataset.title}" de l'auteur "${this.dataset.author}" avec l'éditeur "${this.dataset.publisher}" en format "${this.dataset.format}" et l'État "${this.dataset.state}"`
         });
     }
 }
