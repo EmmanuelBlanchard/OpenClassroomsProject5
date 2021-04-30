6e284e9012dea9a6b7a05ee384781cd085192b1f
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-04-30 15:41:02 +0200

    In src/Controller/Admin/AdminController.php, deleted unnecessary uses.

--- books_exchange/src/Controller/Admin/AdminController.php
+++ books_exchange/src/Controller/Admin/AdminController.php
@@ -2,18 +2,6 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\State;
-use App\Entity\Author;
-use App\Entity\Format;
-use App\Entity\Category;
-use App\Entity\Language;
-use App\Entity\Publisher;
-use App\Form\StateFormType;
-use App\Form\AuthorFormType;
-use App\Form\FormatFormType;
-use App\Form\CategoryFormType;
-use App\Form\LanguageFormType;
-use App\Form\PublisherFormType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
