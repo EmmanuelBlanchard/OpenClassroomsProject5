491fb814777e551b3cc333deef9569d2f611ca04
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-05-05 18:37:37 +0200

    In public/js/modalcontact.js, fix the problem of displaying the book title in the form by retrieving the value of data-title. In templates/book/exchanges.html.twig and templates/book/show.html.twig, updated the 'info' class with the 'primary' class for the <a> tag entitled 'Contacter'.

--- books_exchange/public/js/modalcontact.js
+++ books_exchange/public/js/modalcontact.js
@@ -1,14 +1,5 @@
 window.onload = () => {
-    document.querySelector('#book_contact_form_title').value = `"${this.dataset.title}"`
-
-    document.querySelector('#book_contact_form_title').innerText = `"${this.dataset.title}"`
-
+    let buttonContactBook = document.querySelector('.modal-trigger').getAttribute("data-title");
+    let bookContactFormTitle = document.querySelector('#book_contact_form_title').value = buttonContactBook;
     let modal = new bootstrap.Modal(document.getElementById('modal-contact'));
-
-    let deletion = document.querySelectorAll(".modal-trigger")
-    for (let button of deletion) {
-        button.addEventListener("click", function () {
-            document.querySelector(".modal-footer a").href = ``
-        });
-    }
 }
