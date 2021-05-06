307d2b1710537d463ff1d3f489607f121975c047
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-05-06 16:15:08 +0200

    In UserController.php, lines 153 -328: removal of the methods indexBook, addBook, bookRemoveFromStock, bookAddFromExchanges, bookRemovedFromExchanges, showBooksExchange, validationConfirmBookExchange, confirmBookExchange, bookCancelExchange because put in BookController.php through the methods index,add, removeStock, addExchanges, removeExchanges, exchanges,, validationConfirmExchange, confirmExchange, cancelExchange.

--- books_exchange/src/Controller/UserController.php
+++ books_exchange/src/Controller/UserController.php
@@ -2,10 +2,7 @@
 
 namespace App\Controller;
 
-use App\Entity\Book;
-use App\Form\BookFormType;
 use App\Form\UpdateEmailFormType;
-use App\Repository\BookRepository;
 use App\Form\UpdateProfileFormType;
 use App\Form\UpdatePasswordFormType;
 use Symfony\Component\HttpFoundation\Request;
@@ -26,9 +23,9 @@ class UserController extends AbstractController
         return $this->render('user/index.html.twig');
     }
 
-     /**
-     * @Route("/user/profile/update", name="user_profile_update")
-     */
+    /**
+    * @Route("/user/profile/update", name="user_profile_update")
+    */
     public function updateProfile(Request $request): Response
     {
         $user = $this->getUser();
@@ -88,7 +85,7 @@ class UserController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
-            $user->setEmail (
+            $user->setEmail(
                 $form->get('email')->getData()
             );
             
@@ -118,16 +115,16 @@ class UserController extends AbstractController
     {
         // We define the PDF options
         $pdfOptions = new Options();
-        // Default font 
+        // Default font
         $pdfOptions->set('defaultFont', 'Times-Roman');
         $pdfOptions->isRemoteEnabled(true);
         // We instantiate Dompdf
         $dompdf = new Dompdf($pdfOptions);
         $context = stream_context_create([
             'ssl' => [
-                'verify_peer' => FALSE,
-                'verify_peer_name' => FALSE,
-                'allow_self_signed' => TRUE,
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true,
             ]
         ]);
         $dompdf->setHttpContext($context);
@@ -142,189 +139,11 @@ class UserController extends AbstractController
         // We generate a file name
         $file = 'user-data-'. $this->getUser()->getId() .'.pdf';
 
-        // We send the pdf to the browser 
+        // We send the pdf to the browser
         $dompdf->stream($file, [
             'Attachment' => true
         ]);
 
         return new Response;
     }
-
-    /**
-    * @Route("/user/book", name="user_book")
-    */
-    public function indexBook(): Response
-    {
-        return $this->render('user/book/index.html.twig');
-    }
-
-    /**
-     * @Route("/user/book/add", name="user_book_add")
-     */
-    public function addBook(Request $request): Response
-    {
-        $book = new Book;
-        $form = $this->createForm(BookFormType::class, $book);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $book->setUser($this->getUser());
-            $book->setActive(true);
-            $book->setExchangeRequest(false);
-            $book->setExchangeRequestAt(new \DateTime('0000-00-00 00:00:00'));
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($book);
-            $entityManager->flush();
-
-            $this->addFlash('message', 'Le livre a été ajouté à votre stock !');
-            return $this->redirectToRoute('user_book');
-        }
-
-        return $this->render('user/book/add.html.twig', [
-            'addbookForm' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/user/book/{id}/remove/stock", name="user_book_remove_stock")
-     */
-    public function bookRemoveFromStock(int $id): Response
-    {
-        $repository = $this->getDoctrine()->getRepository(Book::class);
-        $book = $repository->find($id);
-        if($book === null) {
-            // Make a flash bag message
-            $this->addFlash('error', 'Erreur : Aucun livre ne correspond');
-        } else {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->remove($book);
-            $entityManager->flush();
-            $this->addFlash('message', 'Livre supprimé du stock de livres');
-        }
-        return $this->redirectToRoute('user_book');
-    }
-
-    /**
-     * @Route("/user/book/{id}/add/exchanges", name="user_book_add_exchanges")
-     */
-    public function bookAddFromExchanges(int $id): Response
-    {
-        $repository = $this->getDoctrine()->getRepository(Book::class);
-        $book = $repository->find($id);
-        if($book === null) {
-            // Make a flash bag message
-            $this->addFlash('error', 'Erreur : Aucun livre ne correspond');
-        } else {
-            $book->setActive(true);
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($book);
-            $entityManager->flush();
-            $this->addFlash('message', 'Livre ajouté aux échanges');
-        }
-        return $this->redirectToRoute('user_book');
-    }
-
-    /**
-     * @Route("/user/book/{id}/remove/exchanges", name="user_book_remove_exchanges")
-     */
-    public function bookRemovedFromExchanges(int $id): Response
-    {
-        $repository = $this->getDoctrine()->getRepository(Book::class);
-        $book = $repository->find($id);
-        if($book === null) {
-            // Make a flash bag message
-            $this->addFlash('error', 'Erreur : Aucun livre ne correspond');
-        } else {
-            $book->setActive(false);
-            $book->setExchangeRequest(false);
-            $book->setUserexchange(null);
-            $book->setExchangeRequestAt(new \DateTime('0000-00-00 00:00:00'));
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($book);
-            $entityManager->flush();
-            $this->addFlash('message', 'Livre retiré des échanges');
-        }
-        return $this->redirectToRoute('user_book');
-    }
-
-    /**
-     * @Route("/user/book/show/exchange", name="user_book_show_exchange")
-     */
-    public function showBooksExchange(BookRepository $bookRepo): Response
-    {
-        $user = $this->getUser();
-
-        $theBooksIRequestedToExchange = $bookRepo->findBooksActiveWithExchangeRequestRequestedByUser($user);
-
-        $myBooksRequestedForExchange  = $bookRepo->findBooksActiveWithExchangeRequestOwnedByUser($user);
-
-        return $this->render('user/book/showexchange.html.twig', ['theBooksIRequestedToExchange' => $theBooksIRequestedToExchange,'myBooksRequestedForExchange' => $myBooksRequestedForExchange]);
-    }
-
-    /**
-     * @Route("/user/book/{id}/validation/confirm/exchange", name="user_book_validation_confirm_exchange")
-     */
-    public function validationConfirmBookExchange(int $id): Response
-    {
-        $repository = $this->getDoctrine()->getRepository(Book::class);
-        $book = $repository->find($id);
-        if($book === null) {
-            // Make a flash bag message
-            $this->addFlash('error', 'Erreur : problème d\'identification du livre');
-        } else {
-            return $this->render('user/book/validationconfirmbookexchange.html.twig', ['book' => $book]);
-        }
-        return $this->redirectToRoute('user_book');
-    }
-
-    /**
-     * @Route("/user/book/{id}/confirm/exchange", name="user_book_confirm_exchange")
-     */
-    public function confirmBookExchange(int $id): Response
-    {
-        $user = $this->getUser();
-        $repository = $this->getDoctrine()->getRepository(Book::class);
-        $book = $repository->find($id);
-        if($book === null) {
-            // Make a flash bag message
-            $this->addFlash('error', 'Erreur : problème d\'identification du livre');
-        } else {
-            $book->setExchangeRequest(true);
-            $book->setExchangeRequestAt(new \DateTime('now'));
-            $book->setUserexchange($user);
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($book);
-            $entityManager->flush();
-
-            return $this->render('user/book/confirmbookexchange.html.twig', ['book' => $book]);
-        }
-        return $this->redirectToRoute('user_book');
-    }
-
-    /**
-     * @Route("/user/book/{id}/cancel/exchange", name="user_book_cancel_exchange")
-     */
-    public function bookCancelExchange(int $id): Response
-    {
-        $repository = $this->getDoctrine()->getRepository(Book::class);
-        $book = $repository->find($id);
-        if($book === null) {
-            // Make a flash bag message
-            $this->addFlash('error', 'Erreur : Aucun livre ne correspond');
-        } else {
-            $book->setActive(false);
-            $book->setExchangeRequest(false);
-            $book->setUserexchange(null);
-            $book->setExchangeRequestAt(new \DateTime('0000-00-00 00:00:00'));
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($book);
-            $entityManager->flush();
-            $this->addFlash('message', 'Livre retiré de l\'échange de livres');
-        }
-        return $this->redirectToRoute('user_book');
-    }
-
 }
