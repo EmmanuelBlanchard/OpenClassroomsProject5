327a8f7fd9c840bbd13d977199754d33446cb4d0
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-05-06 15:48:04 +0200

    Added Export personal data, In templates/user/index.html.twig, line 52: added a button allowing the user to see his personal data with the 'user_data' route. In src/Controller/UserController.php, lines 16-17: added uses for dompdf, line 29-105: move the methods updateProfile, updatePassword, updateEmail to the top of the UserController.php class, line 106-152: added methods userData, userDataDownload. In public/css/style3.css, line 727: added the display property with the flex value of the 'div.col-sm-12.divButtonsEdit' selector to have the four buttons inline in the div tag. In templates/ folder, added base_pdf.html.twig file to give dompd the base file. In templates/user/ folder, added a data.html.twig file for the userData method to see the user's personal data, then added download.html.twig file for the userDataDownload method for dompdf to generate the html file.

--- books_exchange/src/Controller/UserController.php
+++ books_exchange/src/Controller/UserController.php
@@ -13,6 +13,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 
 class UserController extends AbstractController
 {
@@ -22,6 +24,130 @@ class UserController extends AbstractController
     public function index(): Response
     {
         return $this->render('user/index.html.twig');
+    }
+
+     /**
+     * @Route("/user/profile/update", name="user_profile_update")
+     */
+    public function updateProfile(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UpdateProfileFormType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('message', 'Profil mis à jour');
+            return $this->redirectToRoute('user');
+        }
+        return $this->render('user/updateprofile.html.twig', [
+            'updateProfileForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/user/password/update", name="user_password_update")
+     */
+    public function updatePassword(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UpdatePasswordFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('message', 'Mot de passe mis à jour avec succès');
+            return $this->redirectToRoute('user');
+        }
+        return $this->render('user/updatepassword.html.twig', [
+            'updatePasswordForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/user/email/update", name="user_email_update")
+     */
+    public function updateEmail(Request $request): Response
+    {
+        $user = $this->getUser();
+        $form = $this->createForm(UpdateEmailFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setEmail (
+                $form->get('email')->getData()
+            );
+            
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+            $this->addFlash('message', 'L\'adresse e-mail a été mise à jour avec succès');
+            return $this->redirectToRoute('user');
+        }
+        return $this->render('user/updateemail.html.twig', [
+            'updateEmailForm' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/user/data", name="user_data")
+     */
+    public function userData(): Response
+    {
+        return $this->render('user/data.html.twig');
+    }
+
+    /**
+     * @Route("/user/data/download", name="user_data_download")
+     */
+    public function userDataDownload(): Response
+    {
+        // We define the PDF options
+        $pdfOptions = new Options();
+        // Default font 
+        $pdfOptions->set('defaultFont', 'Times-Roman');
+        $pdfOptions->isRemoteEnabled(true);
+        // We instantiate Dompdf
+        $dompdf = new Dompdf($pdfOptions);
+        $context = stream_context_create([
+            'ssl' => [
+                'verify_peer' => FALSE,
+                'verify_peer_name' => FALSE,
+                'allow_self_signed' => TRUE,
+            ]
+        ]);
+        $dompdf->setHttpContext($context);
+
+        // We generate the html
+        $html = $this->renderView('user/download.html.twig');
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+
+        // We generate a file name
+        $file = 'user-data-'. $this->getUser()->getId() .'.pdf';
+
+        // We send the pdf to the browser 
+        $dompdf->stream($file, [
+            'Attachment' => true
+        ]);
+
+        return new Response;
     }
 
     /**
@@ -199,83 +325,6 @@ class UserController extends AbstractController
             $this->addFlash('message', 'Livre retiré de l\'échange de livres');
         }
         return $this->redirectToRoute('user_book');
-    }
-
-    /**
-     * @Route("/user/profile/update", name="user_profile_update")
-     */
-    public function updateProfile(Request $request): Response
-    {
-        $user = $this->getUser();
-        $form = $this->createForm(UpdateProfileFormType::class, $user);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-            $this->addFlash('message', 'Profil mis à jour');
-            return $this->redirectToRoute('user');
-        }
-        return $this->render('user/updateprofile.html.twig', [
-            'updateProfileForm' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/user/password/update", name="user_password_update")
-     */
-    public function updatePassword(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
-    {
-        $user = $this->getUser();
-        $form = $this->createForm(UpdatePasswordFormType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $passwordEncoder->encodePassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
-            
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-            $this->addFlash('message', 'Mot de passe mis à jour avec succès');
-            return $this->redirectToRoute('user');
-        }
-        return $this->render('user/updatepassword.html.twig', [
-            'updatePasswordForm' => $form->createView()
-        ]);
-    }
-
-    /**
-     * @Route("/user/email/update", name="user_email_update")
-     */
-    public function updateEmail(Request $request): Response
-    {
-        $user = $this->getUser();
-        $form = $this->createForm(UpdateEmailFormType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setEmail (
-                $form->get('email')->getData()
-            );
-            
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
-            $this->addFlash('message', 'L\'adresse e-mail a été mise à jour avec succès');
-            return $this->redirectToRoute('user');
-        }
-        return $this->render('user/updateemail.html.twig', [
-            'updateEmailForm' => $form->createView()
-        ]);
     }
 
 }
