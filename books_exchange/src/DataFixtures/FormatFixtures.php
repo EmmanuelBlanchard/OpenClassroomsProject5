05704272aba36854801a65412391cee230523a03
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-05-11 11:59:30 +0200

    In src/DataFixtures/, updated hte AuthorFixtures.php, FormatFixtures.php, PublisherFixtures.php.

--- books_exchange/src/DataFixtures/FormatFixtures.php
+++ books_exchange/src/DataFixtures/FormatFixtures.php
@@ -15,41 +15,65 @@ class FormatFixtures extends Fixture
                 'name' => '11*17 - Poche'
             ],
             2 => [
-                'name' => '12*18 - Manga'
+                'name' => '11*18 - Poche'
             ],
             3 => [
-                'name' => '11*20 - Romantique'
+                'name' => '12*18 - Manga'
             ],
             4 => [
-                'name' => '14.8*21 - A5'
+                'name' => '13*18'
             ],
             5 => [
-                'name' => '16*25 - Comic US'
+                'name' => '11*20 - Romantique'
             ],
             6 => [
-                'name' => '18*26 - MDO'
+                'name' => '13*20 - Grand Poche'
             ],
             7 => [
-                'name' => '21*29.7 - A4'
+                'name' => '14*20  - Broché'
             ],
             8 => [
-                'name' => '15*15 - Carré'
+                'name' => '14.8*21 - A5'
             ],
             9 => [
-                'name' => '21*21 - Grand Carré'
+                'name' => '15*22 - Grand Poche'
             ],
             10 => [
-                'name' => '19*15 - Panoramique'
+                'name' => '16*25 - Comic US'
             ],
             11 => [
-                'name' => '29.7*21 - A4 Paysage'
+                'name' => '18*26 - MDO'
             ],
             12 => [
+                'name' => '21*29.7 - A4'
+            ],
+            13 => [
+                'name' => '15*15 - Carré'
+            ],
+            14 => [
+                'name' => '16*23 - '
+            ],
+            15 => [
+                'name' => '16*24 - Royal'
+            ],
+            16 => [
+                'name' => '21*21 - Grand Carré'
+            ],
+            17 => [
+                'name' => '19*15 - Panoramique'
+            ],
+            18 => [
+                'name' => '24*32 - BD'
+            ],
+            19 => [
+                'name' => '29.7*21 - A4 Paysage'
+            ],
+            20 => [
                 'name' => 'Autre'
             ],
         ];
 
-        foreach($format as $key => $value) {
+        foreach ($format as $key => $value) {
             $format = new Format();
             $format->setName($value['name']);
             $manager->persist($format);
