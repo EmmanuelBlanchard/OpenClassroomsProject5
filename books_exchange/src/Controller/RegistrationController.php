dcb93320d1f5850b36df47850b5d488867bba6c7
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-05-17 15:28:10 +0200

    In src/Controller/RegistrationController.php, lines 32-35: route register refused if the user has the ROLE_USER, line 103:  if e-mail verification is ok, change the redirection to success on the app_login route.

--- books_exchange/src/Controller/RegistrationController.php
+++ books_exchange/src/Controller/RegistrationController.php
@@ -29,6 +29,11 @@ class RegistrationController extends AbstractController
      */
     public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+        if ($this->isgranted('ROLE_USER')) {
+            $this->addFlash('error', 'Accès à la page de création de compte refusé. Déconnectez-vous pour avoir accès à la page ce création de compte.');
+            return $this->redirectToRoute('app_home');
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -47,11 +52,13 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $user,
                 (new TemplatedEmail())
                     ->from(new Address('no-reply@booksexchange.test', 'BooksExchange.test'))
                     ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
+                    ->subject('Veuillez confirmer votre adresse e-mail')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
             // do anything else you need here, like send an email
@@ -93,6 +100,6 @@ class RegistrationController extends AbstractController
         // @TODO Change the redirect on success and handle or remove the flash message in your templates
         $this->addFlash('success', 'Your email address has been verified.');
 
-        return $this->redirectToRoute('app_register');
+        return $this->redirectToRoute('app_login');
     }
 }
