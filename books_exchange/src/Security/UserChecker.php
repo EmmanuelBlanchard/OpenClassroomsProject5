68dd31dbe68bbd9f5141d9da1c70295da754dd7f
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-05-17 18:32:08 +0200

    In  src/Security/UserAuthenticator.php, line 74: customized message translation. In src/Security/UserChecker.php, lines 16-22: added EmailVerifier class in the constructor of the UserChecher class, lines 35-44: send back an e-mail confirmation of his e-mail, if the user has not validated his e-mail. In  templates/registration/confirmation_email.html.twig, removed typo. In templates/security/login.html.twig, lines 18-22: move the flash message block below the if error block then delete the block if app.user.

--- books_exchange/src/Security/UserChecker.php
+++ books_exchange/src/Security/UserChecker.php
@@ -3,13 +3,23 @@
 namespace App\Security;
 
 use App\Entity\User as AppUser;
+use App\Security\EmailVerifier;
+use Symfony\Component\Mime\Address;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
 use Symfony\Component\Security\Core\Exception\AccountExpiredException;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
-use Symfony\Component\Security\Core\User\UserCheckerInterface;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserChecker implements UserCheckerInterface
 {
+    private $emailVerifier;
+
+    public function __construct(EmailVerifier $emailVerifier)
+    {
+        $this->emailVerifier = $emailVerifier;
+    }
+
     public function checkPreAuth(UserInterface $user): void
     {
         if (!$user instanceof AppUser) {
@@ -22,6 +32,16 @@ class UserChecker implements UserCheckerInterface
         }*/
 
         if (!$user->isVerified()) {
+            $this->emailVerifier->sendEmailConfirmation(
+                'app_verify_email',
+                $user,
+                (new TemplatedEmail())
+                    ->from(new Address('no-reply@booksexchange.test', 'BooksExchange.test'))
+                    ->to($user->getEmail())
+                    ->subject('Veuillez confirmer votre adresse e-mail')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
+
             throw new CustomUserMessageAccountStatusException('Votre compte n\'est pas activé. Veuillez confirmer 
             votre inscription en cliquant sur le lien qui vous a été envoyé par email. Pensez à vérifier dans vos spams.');
         }
