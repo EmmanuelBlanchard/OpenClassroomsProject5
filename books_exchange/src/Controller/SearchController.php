608df61b810d69795641c0b3d953721ffe353c40
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-05-20 20:51:12 +0200

    In src/Controller/SearchController.php, updated index method with test search work but no category select search... . In templates/main/ folder, added temprory search_books_test.html.twig file.

--- books_exchange/src/Controller/SearchController.php
+++ books_exchange/src/Controller/SearchController.php
@@ -2,23 +2,41 @@
 
 namespace App\Controller;
 
+use App\Entity\Book;
+use App\Repository\BookRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 
 class SearchController extends AbstractController
 {
     /**
      * @Route("/search", name="app_search")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, BookRepository $bookRepo): Response
     {
         $search = $request->query->get('q');
         $search = trim($search);
+        
+        $repository = $this->getDoctrine()->getRepository(Book::class);
+
+        $books = $repository->findAll($search);
+
+        //$search = $request->query->get('q');
+        //dd($books);
 
         if ($search === null || $search === '') {
             return $this->redirectToRoute('app_home');
+        } else {
+            $books = $bookRepo->search(
+                $search = $request->query->get('q'),
+                $search = $request->query->get('category')
+            );
         }
+
+        return $this->render('main/search_books_test.html.twig', [
+            'books' => $books,
+        ]);
     }
 }
