5a5910e7616266d25ab1d29294f2db5e81702d35
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-05-26 16:21:17 +0200

    In src/Controller/MainController.php, lines 18-36: reorganization of the code in the index method. In templates/main/_content_flex.html.twig, lines 50-81: add a condition if , if the number of pages is greater than 1 the code of the pagination is used and in the case where the maximum number of pages is 1 then no pagination.

--- books_exchange/src/Controller/MainController.php
+++ books_exchange/src/Controller/MainController.php
@@ -2,13 +2,10 @@
 
 namespace App\Controller;
 
-use App\Form\SearchBookFormType;
 use App\Repository\BookRepository;
-use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class MainController extends AbstractController
@@ -18,39 +15,25 @@ class MainController extends AbstractController
      */
     public function index(Request $request, BookRepository $bookRepo): Response
     {
+        // We get the information of the connected user
+        $user = $this->getUser();
         // We define the number of books per page
         $limit = 10;
         // We get the page number
         $page = (int)$request->query->get("page", 1);
-        // We get the books of the page according to the filter
-        $books = $bookRepo->getPaginatedBooksNoUser($page, $limit);
-        // We get the total number of books according to the filter
-        $total = $bookRepo->getTotalBooksActiveWithoutExchangeRequest();
-        // How many pages will there be
-        $pages = ceil($total / $limit);
-
-        if ($this->getUser()) {
-            // We get the information of the connected user
-            $user = $this->getUser();
-            // We define the number of books per page
-            $limit = 10;
-            // We get the page number
-            $page = (int)$request->query->get("page", 1);
+        if ($user) {
             // We get the books of the page according to the filter
             $books = $bookRepo->getPaginatedBooks($page, $limit, $user);
             // We get the total number of books according to the filter
             $total = $bookRepo->getTotalBooksActiveWithoutExchangeRequestNotOwnedByUser($user);
-            // How many pages will there be
-            $pages = ceil($total / $limit);
-
-            return $this->render('main/index.html.twig', [
-                'books' => $books,
-                'limit' => $limit,
-                'page' => $page,
-                'pages' => $pages,
-                'total' => $total,
-            ]);
+        } elseif (!$user) {
+            // We get the books of the page according to the filter
+            $books = $bookRepo->getPaginatedBooksNoUser($page, $limit);
+            // We get the total number of books according to the filter
+            $total = $bookRepo->getTotalBooksActiveWithoutExchangeRequest();
         }
+        // How many pages will there be
+        $pages = (int)ceil($total / $limit);
 
         return $this->render('main/index.html.twig', [
             'books' => $books,
