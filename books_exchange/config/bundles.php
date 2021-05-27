c5c4b9b4a82f04750a049cf2d074647f2674d422
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-05-27 22:36:40 +0200

    Install the Zenstruck Foundry library

--- books_exchange/config/bundles.php
+++ books_exchange/config/bundles.php
@@ -18,4 +18,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
