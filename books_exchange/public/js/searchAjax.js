8f61d9ab8d470b46799f34787ef72eb8a20c9819
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-07-07 21:19:27 +0200

    In public/js/searchAjax.js, line 4: updated the const searchForm by the const searchFormInput, line 5: added the property ajaxurl, line 6: updated the element listening (searchFormInput). In templates/base.html.twig, line 72: added the identifiant searchFormInput for the <input> tag.

--- books_exchange/public/js/searchAjax.js
+++ books_exchange/public/js/searchAjax.js
@@ -1,8 +1,9 @@
 class SearchAjax {
     constructor() {
         this.divResultSearch = document.getElementById('resultSearch');
-        const searchForm = document.getElementById('searchForm');
-        searchForm.addEventListener('input', event => {
+        const searchFormInput = document.getElementById('searchFormInput');
+        this.ajaxurl = searchFormInput.dataset.ajaxurl;
+        searchFormInput.addEventListener('input', event => {
             const data = event.currentTarget.value;
             console.log(data);
         });
