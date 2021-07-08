3c9619e0596cbfaa982aac00995c417a463f39cc
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-07-08 16:31:22 +0200

    In src/Controller/SearchController.php, deleted unnecessary use;

--- books_exchange/src/Controller/SearchController.php
+++ books_exchange/src/Controller/SearchController.php
@@ -2,12 +2,10 @@
 
 namespace App\Controller;
 
-use App\Entity\Book;
 use App\Repository\BookRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SearchController extends AbstractController
