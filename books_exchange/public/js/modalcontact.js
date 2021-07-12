f883033555c65c5b6f0493f203fc5ce263c6878d
EmmanuelBlanchard <emmanuel.blanchard@tutanota.com>
2021-07-12 21:23:26 +0200

    In public/js/ folder, test deletion of the modalcontact.js file in order to rename it to camelCase.

--- books_exchange/public/js/modalcontact.js
+++
@@ -1,5 +0,0 @@
-window.onload = () => {
-    let buttonContactBook = document.querySelector('.modal-trigger').getAttribute("data-title");
-    let bookContactFormTitle = document.querySelector('#book_contact_form_title').value = buttonContactBook;
-    let modal = new bootstrap.Modal(document.getElementById('modal-contact'));
-}
